34f2a34111b23794580d668a6a1d9cc74c039e02
Yoseph Anderson <yrganderson@gmail.com>
2016-12-07 17:44:54 -0700

    maping dispatch to container

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import { selectBook } from '../actions/index';
+import { bindActionsCreators } from 'redux';
 
 class BookList extends Component {
   _renderList() {
@@ -25,4 +27,8 @@ function mapStateToProps(state) {
   });
 }
 
-export default connect(mapStateToProps)(BookList);
+function mapDispatchToProps() {
+  return bindActionsCreators({ selectBook: selectBook }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
