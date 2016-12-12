421d544d44390aeee13e77e884d2e388092036df
Yoseph Anderson <yrganderson@gmail.com>
2016-12-11 20:10:51 -0700

    book detail container * active book state * connected to reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,7 @@ import ActiveBook from './reducer_active_book';
 
 const rootReducer = combineReducers({
   books: BooksReducer,
-  activeBook: activeBook
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
